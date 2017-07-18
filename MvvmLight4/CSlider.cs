1083c323f07a05f4de4a83f2791e5754435ea4e3
Chanhun Lee <chanhun.lee@meshkorea.net>
2017-07-18 16:13:37 +0900

    ret

---
+++ MvvmLight4/CSlider.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace MvvmLight4
+{
+    class CSlider : System.Windows.Controls.Slider
+    {
+        #region Private Members 
+
+        private Thumb thumb = null;
+        private bool isDownOnSlider = false;
+
+        #endregion
+
+        #region override Member 
+        public CSlider()
+        {
+        }
+
+        //private void CSlider_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        //{
+        //    var _thumb = (GetTemplateChild("PART_Track") as Track).Thumb;
+        //    var _repeatDec = (GetTemplateChild("PART_Track") as Track).DecreaseRepeatButton;
+
+        //    thumb.Width = e.NewSize.Width / (Maximum + 1);
+
+        //    _repeatDec.Margin = new System.Windows.Thickness(0, 0, thumb.Width * -1, 0);
+        //}
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (thumb != null)
+            {
+                thumb.MouseEnter -= thumb_MouseEnter;
+                thumb.LostMouseCapture -= thumb_LostMouseCapture;
+            }
+
+            thumb = (GetTemplateChild("PART_Track") as Track).Thumb;
+
+            if (thumb != null)
+            {
+                thumb.MouseEnter += thumb_MouseEnter;
+                thumb.LostMouseCapture += thumb_LostMouseCapture;
+            }
+        }
+
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+            isDownOnSlider = true;
+        }
+
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            if (Mouse.LeftButton != MouseButtonState.Pressed)
+            {
+                base.OnValueChanged(oldValue, newValue);
+            }
+        }
+
+        #endregion
+        #region Private handler 
+
+        private void thumb_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && isDownOnSlider)
+            {
+                // the left button is pressed on mouse enter 
+                // so the thumb must have been moved under the mouse 
+                // in response to a click on the track. 
+                // Generate a MouseLeftButtonDown event. 
+                MouseButtonEventArgs args = new MouseButtonEventArgs(
+                    e.MouseDevice, e.Timestamp, MouseButton.Left);
+                args.RoutedEvent = MouseLeftButtonDownEvent;
+                (sender as Thumb).RaiseEvent(args);
+            }
+        }
+        private void thumb_LostMouseCapture(object sender, EventArgs e)
+        {
+            isDownOnSlider = false;
+            base.OnValueChanged(0, this.Value);
+        }
+        #endregion
+
+
+
+        
+
+    }
+
+}
--- MvvmLight4/MainWindow.xaml.cs
+++ MvvmLight4/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using MvvmLight4.ViewModel;
+using System.Runtime.InteropServices;
+using System;
+using System.Windows.Interop;
 
 namespace MvvmLight4
 {
@@ -8,13 +11,62 @@ namespace MvvmLight4
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern uint RegisterWindowMessage(string lpString);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, uint wParam, uint lParam);
+
+        public const uint HWND_BROADCAST = 0xffff;
+
+        private uint message;
+        private IntPtr handle;
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
         /// </summary>
         public MainWindow()
         {
+
             InitializeComponent();
             Closing += (s, e) => ViewModelLocator.Cleanup();
+
+            
+
+            handle = new WindowInteropHelper(this).Handle;
+            message = RegisterWindowMessage("User Message");
+            ComponentDispatcher.ThreadFilterMessage += ComponentDispatcher_ThreadFilterMessage;
+            MessageBox.Show(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        }
+        private void ComponentDispatcher_ThreadFilterMessage(ref MSG msg, ref bool handled)
+        {
+            if (msg.message == message && msg.wParam != handle)
+            {
+                MessageBox.Show("MFC Message : " + msg.lParam.ToString());
+            }
+
+
+        }
+
+        private void cSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(textBlock1!=null)
+            textBlock1.Text = e.NewValue.ToString();
+            System.Console.WriteLine(e.NewValue.ToString());
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            cSlider.Value -= 1;
+        }
+
+
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            PostMessage((IntPtr)HWND_BROADCAST, message, (uint)handle, 100);
         }
     }
+
 }
